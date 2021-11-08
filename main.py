264e9de0670b991de55c616a2e6d9f86ea7a43c9
hecuhecu <hecunyuji5@gmail.com>
2021-11-08 11:08:55 +0900

    署名の追加を関数化

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def create_message(to, sub, body, sender):
 
     return msg
 
+def add_signature(body):
+    signature = "\n\n" + "---------------------------------" + "\n" + "関西学院大学 理工学部 情報科学科 3年" + "\n" \
+                + "河村 宇記（Hiroki Kawamura）" + "\n" + "Tel: 070-5662-5782" + "\n" + "Mail: hecunyuji5@gmail.com" \
+                + "\n" + "---------------------------------"
+    body += signature
+
+    return body
+
 def send_message(msg, id, pw):
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context())
     server.login(id, pw)
@@ -29,10 +37,7 @@ def main():
     body = "\n".join(iter(input, "exit"))
 
     #署名の追加
-    signature = "\n\n" + "---------------------------------" + "\n" + "関西学院大学 理工学部 情報科学科 3年" + "\n" \
-                + "河村 宇記（Hiroki Kawamura）" + "\n" + "Tel: 070-5662-5782" + "\n" + "Mail: hecunyuji5@gmail.com" \
-                + "\n" + "---------------------------------"
-    body += signature
+    body = add_signature(body)
 
     #メール作成
     msg = create_message(to, subject, body, gmail_account)
